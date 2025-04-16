1ed54fe6031934201b7f0ddc9c7e0919b5a066cd
nevo0309 <nevo19970309@gmail.com.com>
2025-04-16 17:41:16 +0300

    Add Appheader and toyindex to the app

---
+++ src/root-cmp.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router } from 'react-router'
+import { Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import { ToyIndex } from './pages/toyIndex'
+import { AppHeader } from './cmps/AppHeader'
+
+export function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <section className="app">
+          <AppHeader />
+          <main className="main-layout">
+            <Route element={ToyIndex} path="/toy" />
+          </main>
+        </section>
+      </Router>
+    </Provider>
+  )
+}
