ddbb19d07930d1d52d7684f2f74b4f00250b4f49
nevo0309 <nevo19970309@gmail.com.com>
2025-04-16 19:52:23 +0300

    Render toy info

---
+++ src/cmps/ToyPreview.jsx
@@ -0,0 +1,26 @@
+export function ToyPreview({ toy }) {
+  return (
+    <article className="toy-preview">
+      <h3 className="toy-name">{toy.name}</h3>
+      <p className="toy-price">
+        Price: <span>${toy.price.toLocaleString()}</span>
+      </p>
+      <div className="toy-labels">
+        Labels:
+        <div className="labels-container">
+          {toy.labels.map(label => (
+            <span className="label" key={label}>
+              {label}
+            </span>
+          ))}
+        </div>
+      </div>
+      <hr />
+      <div className="action-links">
+        <button className="action-btn">Edit</button>
+        <button className="action-btn">Details</button>
+        <button className="action-btn">Delete</button>
+      </div>
+    </article>
+  )
+}
