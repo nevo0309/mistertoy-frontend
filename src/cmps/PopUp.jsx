09afc408515d597b04d3ccebf1f97782e65baac2
nevo0309 <nevo19970309@gmail.com.com>
2025-04-28 13:01:33 +0300

    Add popup button for chat and details

---
+++ src/cmps/PopUp.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+
+export function PopUp({ header, footer, children, isOpen = false, onClose = () => {} }) {
+  const [isOpenPopUp, setIsOpenPopUp] = useState(isOpen)
+
+  useEffect(() => {
+    setIsOpenPopUp(isOpen)
+  }, [isOpen])
+
+  /////////////////////////////// close on esc/////////////////////
+
+  useEffect(() => {
+    function handleKeyDown(ev) {
+      if (ev.key === 'Escape') {
+        onClose()
+      }
+    }
+
+    if (isOpenPopUp) {
+      window.addEventListener('keydown', handleKeyDown)
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [isOpenPopUp, onClose])
+  //////////////////////////////////////////////////////////////////
+
+  function onClosePopUp() {
+    setIsOpenPopUp(false)
+    onClose()
+  }
+
+  if (!isOpenPopUp) return null
+
+  return (
+    <div onClick={onClosePopUp} className="popup-backdrop">
+      <div onClick={ev => ev.stopPropagation()} className="popup-container">
+        {header && <header className="popup-header">{header}</header>}
+        <main className="popup-main">{children}</main>
+
+        {footer && <footer className="popup-footer">{footer}</footer>}
+      </div>
+    </div>
+  )
+}
